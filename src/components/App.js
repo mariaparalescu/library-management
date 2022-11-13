bda75c2f2371bd3e0eec8fa131d4366829201cb3
maria.paralescu <maria.paralescu@everymatrix.com>
2022-11-13 14:52:50 +0200

    customization of side navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React, {Fragment} from 'react';
 import NavBar from './NavBar';
 import {ThemeProvider} from "@strapi/design-system/ThemeProvider"
 import {lightTheme} from "@strapi/design-system/themes"
-
+import {Crumb, Breadcrumbs, Box} from '@strapi/design-system';
+import { BaseHeaderLayout, HeaderLayout } from '@strapi/design-system/Layout';
 
 function App (){
     return(
         <Fragment>
             <ThemeProvider theme={lightTheme}>
-                <NavBar />
+                    <NavBar />
             </ThemeProvider>
         </Fragment>
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,22 +8,23 @@ import {
     NavUser,
     NavLink,
 } from '@strapi/design-system/MainNav';
-import {Divider, Box} from '@strapi/design-system';
+import {Divider, Box, Breadcrumbs, Crumb} from '@strapi/design-system';
 import {Write, Layer, Landscape, Information, Puzzle, ShoppingCart, Cog} from '@strapi/icons';
+import {BaseHeaderLayout} from "@strapi/design-system/Layout";
 
 const NavBar = () => {
     const [condensed, setCondensed] = useState(false);
     return <Box background="neutral100"  style={{
-        height: '100vh'
+        height: '100vh', display: 'flex'
     }}>
         <MainNav condensed={condensed}>
             <NavBrand workplace="Workplace" title="Library dashboard" icon={<i className="book icon violet"></i>} />
             <Divider />
             <NavSections>
                 <NavLink to="/laa" icon={<Write />} className="active">
-                    Content-type-builder
+                    Home
                 </NavLink>
-                <NavSection label="Plugins">
+                <NavSection label="Books">
                     <NavLink to="/" icon={<Layer />}>
                         Builder
                     </NavLink>
@@ -34,7 +35,7 @@ const NavBar = () => {
                         Documentation
                     </NavLink>
                 </NavSection>
-                <NavSection label="General">
+                <NavSection label="Users">
                     <NavLink to="/builder" icon={<Puzzle />}>
                         Plugins
                     </NavLink>
@@ -47,12 +48,16 @@ const NavBar = () => {
                 </NavSection>
             </NavSections>
             <NavUser src="https://avatars.githubusercontent.com/u/3874873?v=4" to="/somewhere-i-belong">
-                John Duff
+                Admin
             </NavUser>
             <NavCondense onClick={() => setCondensed(s => !s)}>
                 {condensed ? 'Expanded the navbar' : 'Collapse the navbar'}
             </NavCondense>
         </MainNav>
+            <BaseHeaderLayout title="Media Library" subtitle={<Breadcrumbs label="folders">
+                <Crumb>Animals</Crumb>
+                <Crumb>Cats</Crumb>
+            </Breadcrumbs>} as="h2" />
     </Box>;
 }
 
